9e9a3a18a84172898ee10775d93013c5a0741cff
dldnjswns31 <dldnjswns32@gmail.com>
2022-11-27 00:36:51 +0900

    feat: ThemeProvider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import styled from "styled-components";
+
+const StTitle = styled.h1`
+  color: ${({ theme }) => theme.textColor};
+`;
+
+const StWrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.backgroundColor};
+`;
+
 function App() {
   return <div>hello</div>;
 }
